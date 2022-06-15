2cc878fe1dadce6c4aa0fe67c6d978912ccae9c3
Villager <deiondrae@gmail.com>
2022-06-14 20:00:06 -0400

    finished functionality, tasks now persist, added commments to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,48 @@
 const date = moment().format("dddd, MMM Do");
 const hour = parseInt(moment().hour());
-const currentDate = document.getElementById("currentDay")
-const taskArea = document.querySelectorAll("textarea")
-taskAreaArr = (Array.from(taskArea))
-currentDate.innerHTML = date
+const currentDate = document.getElementById("currentDay");
+const taskArea = document.querySelectorAll("textarea");
+const btn = document.querySelectorAll("saveBtn");
+//convert the NodeList from querySelectorAll to an array
+taskAreaArr = (Array.from(taskArea));
+//Set current date on load
+currentDate.innerHTML = date;
 
-
+// loop through array of textareas and update classes based on current time relative to time block of the div
 function checkTasks(taskAreaArr) {
     for (let i = 0; i < taskAreaArr.length; i++) {
-        timeId = parseInt(taskAreaArr[i].id)
+        timeId = parseInt(taskAreaArr[i].id);
         if (moment(timeId).isBefore(hour)) {
-            taskAreaArr[i].classList.add("past")
+            taskAreaArr[i].classList.add("past");
         }
         else if (moment(timeId).isSame(hour)) {
-            taskAreaArr[i].classList.add("present")
+            taskAreaArr[i].classList.add("present");
         }
         else if (moment(timeId).isAfter(hour)) {
-            taskAreaArr[i].classList.add("future")
+            taskAreaArr[i].classList.add("future");
         }
     }
 }
-
+// refresh page every 5 mins 
 setInterval(() => {
     location.reload();
-}, 3600000); 
-//
-checkTasks(taskAreaArr);
+}, 300000); 
 
-function savingTasks(taskAreaArr) {
+// loop through task array and set each time block's tasks to localstorage
+function saveTasks(taskAreaArr){
     for (let i = 0; i < taskAreaArr.length; i++) {
-        timeId = parseInt(taskAreaArr[i].id)
-        }
-}
-    
-function saveTasks(id){
-    for (let i = )
-    localStorage.setItem(taskAreaArr[i], taskAreaArr[i].value )
+        localStorage.setItem(taskAreaArr[i].id, taskAreaArr[i].value);
+    }
 } 
+// loop through task array and pull task values from localstorage into the appropriate code block
+function getTasks(taskAreaArr) {
+    for (let i = 0; i < taskAreaArr.length; i++) {
+        timeId = parseInt(taskAreaArr[i].id);
+        keys = localStorage.getItem(timeId);
+        taskAreaArr[i].value = keys;
+    }
+}
+
+getTasks(taskAreaArr);
+checkTasks(taskAreaArr);
+$(".saveBtn").on("click", () => {saveTasks(taskAreaArr)});
